f8541184f718a3e2078d977a938f326af2607645
JiangXavier <JiangXavier@163.com>
2023-10-24 11:53:35 +0800

    2

--- leetcode100/1-两数之和.go
+++ leetcode100/1-两数之和.go
@@ -11,3 +11,15 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
+
+//暴力
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
